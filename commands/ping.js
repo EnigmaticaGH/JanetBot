f8cfeb343d4c4bf89eb597a8c273e9ada61f79cf
EnigmaticaGH <eventhorizonsoundtracks@gmail.com>
2021-08-19 16:37:55 -0400

    test command options

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
+    .addStringOption(option => option.setName('option1').setDescription('Test Option Please Ignore'))
     .setDescription('Replies with Pong!'),
   async execute(interaction) {
-    await interaction.reply('Pong!');
+    console.log(interaction.options.get('option1'));
+    await interaction.reply(`Pong!`);
   },
 };
